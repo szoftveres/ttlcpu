a534eb92c77c0123e4f7940ebe7f33d77ee4792a
Szoftveres <my@email.com>
2015-07-22 22:37:15 -0700

    fsk improvements

--- fsk/fsk.c
+++ fsk/fsk.c
@@ -2,49 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SAMPLE_RATE     (44100)
+#define SAMPLE_RATE     (30000)
+#define BAUD_RATE       (300)
+
+#define CYCLE_LO        (9)
+#define CYCLE_HI        (11)
 
 static unsigned char data[] = {
-    'a',
-    'G',
-    '3',
-    'd',
-    'v',
-    'V',
-    'e',
-    'G',
-    'o',
-    'O',
-    '6',
-    '7',
-    '5',
+
+#include "../am/bytecode.h"
+
 };
 
 
 void
-gen_freq (int smp_rate, int freq, int period) {
-    int halfcycle;
+gen_freq (int smp_rate, int baud, int cycle) {
+    int num_smp;
+    int freq;
     int i;
     int p;
-    
-    halfcycle = ((((smp_rate * 10) / freq) + 5) / 20);
 
-//    fprintf(stderr, "smp:%u, freq:%u, period:%u, halfcycle:%u\n", smp_rate, freq, period, halfcycle);
+    freq = baud * cycle;
+    num_smp = ((((smp_rate * 10) / freq) + 5) / 10);
 
-    for (p = 0; p != period; p++) {
-        for (i = 0; i != halfcycle; i++) {
-            putchar(0x70);
+    for (p = 0; p != cycle; p++) {
+        for (i = 0; i != (num_smp/2); i++) {
+            putchar(-63);
         }
-    
-        for (i = 0; i != halfcycle; i++) {
-            putchar(0x00);
+
+        for (i = 0; i != (num_smp/2); i++) {
+            putchar(64);
+        }
+        if (num_smp%2) {
+            putchar(0);
         }
     }
 
 }
 
-#define BIT_ONE() gen_freq(SAMPLE_RATE, 3300, 11)
-#define BIT_ZERO() gen_freq(SAMPLE_RATE, 2700, 9)
+void
+baud_stat (int smp_rate, int baud, int cycle) {
+    int num_smp;
+    int freq;
+    int achieved_baud;
+
+    freq = baud * cycle;
+    num_smp = ((((smp_rate * 10) / freq) + 5) / 10);
+    achieved_baud = (((smp_rate * 10) / (num_smp) / cycle) + 5 ) / 10;
+
+    fprintf(stderr, "Statistics:\n  desired_baud:%u\n  smp_rate:%uHz\n  "
+            "freq:%uHz\n  cycle:%u\n  num_smp:%u\n  achieved_baud:%u\n  "
+            "deviation:%d\%\n\n\n",
+            baud,
+            smp_rate,
+            freq,
+            cycle,
+            num_smp,
+            achieved_baud,
+            ((((achieved_baud * 1000) / baud) + 5)/10)-100);
+
+}
+
+
+#define BIT_ZERO() gen_freq(SAMPLE_RATE, BAUD_RATE, CYCLE_LO)
+#define BIT_ONE() gen_freq(SAMPLE_RATE, BAUD_RATE, CYCLE_HI)
+
+#define STAT_ZERO() baud_stat(SAMPLE_RATE, BAUD_RATE, CYCLE_LO)
+#define STAT_ONE() baud_stat(SAMPLE_RATE, BAUD_RATE, CYCLE_HI)
+
+
 
 void
 put_byte (unsigned char b) {
@@ -65,16 +91,26 @@ int
 main (int argc, char** argv) {
     int i;
 
-    for (i = 0; i != 900; i++) {
-        BIT_ONE();              /* 3 sec lead-up */
+    STAT_ZERO();
+    STAT_ONE();
+
+    for (i = 0; i != 600; i++) {
+        BIT_ONE();              /* 2 sec lead-in */
     }
-    for (i = 0; i != 256; i++) {
+    for (i = 0; i != (sizeof(data)/sizeof(data[0])); i++) {
+        if (!(i%256)) {
+            int k;
+            for (k = 0; k != 75; k++) {
+                BIT_ONE();      /* 0.25 sec verify */
+            }
+        }
+
         BIT_ZERO();             /* Start bit */
-        put_byte(i);
+        put_byte(data[i]);
         BIT_ONE();              /* Stop bit */
-    } 
-    for (i = 0; i != 900; i++) {
-        BIT_ONE();              /* 3 sec ramp-down */
+    }
+    for (i = 0; i != 600; i++) {
+        BIT_ONE();              /* 2 sec lead-out */
     }
 }
 
