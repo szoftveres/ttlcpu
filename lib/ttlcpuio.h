31c46bca31cad155fa6941aabf5de29c431aff3d
szoftveres <szoftveres@email.com>
2016-07-12 16:37:10 -0700

    Added ttlcpuio.h Bracket symbols

---
+++ lib/ttlcpuio.h
@@ -0,0 +1,62 @@
+#ifndef __TTLCPUIO_H__
+#define __TTLCPUIO_H__
+
+
+#define SEGMENT_A           (0x10)
+#define SEGMENT_B           (0x08)
+#define SEGMENT_C           (0x40)
+#define SEGMENT_D           (0x02)
+#define SEGMENT_E           (0x80)
+#define SEGMENT_F           (0x04)
+#define SEGMENT_G           (0x01)
+#define SEGMENT_DP          (0x20)
+
+
+#define SYM_DP              (0x20)
+#define SYM_HYPHEN          (0x01)
+#define SYM_UNDERSCORE      (0x0D)
+#define SYM_SPACE           (0x00)
+#define SYM_COMMA           (0x80)
+#define SYM_LEFT_BRACKET    (0x96)
+#define SYM_RIGHT_BRACKET   (0x5A)
+
+#define SYM_0           (0xDE)
+#define SYM_1           (0x48)
+#define SYM_2           (0x9B)
+#define SYM_3           (0x5B)
+#define SYM_4           (0x4D)
+#define SYM_5           (0x57)
+#define SYM_6           (0xD7)
+#define SYM_7           (0x58)
+#define SYM_8           (0xDF)
+#define SYM_9           (0x5F)
+#define SYM_A           (0xDD)
+#define SYM_B           (0xC7)
+#define SYM_C           (0x96)
+#define SYM_D           (0xCB)
+#define SYM_E           (0x97)
+#define SYM_F           (0x95)
+
+#define SYM_G           (0xD6)
+#define SYM_H           (0xCD)
+#define SYM_I           (0x48)
+#define SYM_J           (0xCA)
+#define SYM_K           (0x00)  /* unimplemented */
+#define SYM_L           (0x86)
+#define SYM_M           (0x00)  /* unimplemented */
+#define SYM_N           (0xC1)
+#define SYM_O           (0xDE)
+#define SYM_P           (0x9D)
+#define SYM_Q           (0x5D)
+#define SYM_R           (0x81)
+#define SYM_S           (0x57)
+#define SYM_T           (0x87)
+#define SYM_U           (0xCE)
+#define SYM_V           (0x00)  /* unimplemented */
+#define SYM_W           (0x00)  /* unimplemented */
+#define SYM_X           (0x00)  /* unimplemented */
+#define SYM_Y           (0x4F)
+#define SYM_Z           (0x00)  /* unimplemented */
+
+
+#endif
--- misc/prog.c
+++ misc/prog.c
@@ -1,58 +1,5 @@
 
-#define SEGMENT_A       (0x10)
-#define SEGMENT_B       (0x08)
-#define SEGMENT_C       (0x40)
-#define SEGMENT_D       (0x02)
-#define SEGMENT_E       (0x80)
-#define SEGMENT_F       (0x04)
-#define SEGMENT_G       (0x01)
-#define SEGMENT_DP      (0x20)
-
-
-#define SYM_DP          (0x20)
-#define SYM_HYPHEN      (0x01)
-#define SYM_UNDERSCORE  (0x0D)
-#define SYM_SPACE       (0x00)
-#define SYM_COMMA       (0x80)
-
-#define SYM_0           (0xDE)
-#define SYM_1           (0x48)
-#define SYM_2           (0x9B)
-#define SYM_3           (0x5B)
-#define SYM_4           (0x4D)
-#define SYM_5           (0x57)
-#define SYM_6           (0xD7)
-#define SYM_7           (0x58)
-#define SYM_8           (0xDF)
-#define SYM_9           (0x5F)
-#define SYM_A           (0xDD)
-#define SYM_B           (0xC7)
-#define SYM_C           (0x96)
-#define SYM_D           (0xCB)
-#define SYM_E           (0x97)
-#define SYM_F           (0x95)
-
-#define SYM_G           (0xD6)
-#define SYM_H           (0xCD)
-#define SYM_I           (0x48)
-#define SYM_J           (0xCA)
-#define SYM_K           (0x00)  /* unimplemented */
-#define SYM_L           (0x86)
-#define SYM_M           (0x00)  /* unimplemented */
-#define SYM_N           (0xC1)
-#define SYM_O           (0xDE)
-#define SYM_P           (0x9D)
-#define SYM_Q           (0x5D)
-#define SYM_R           (0x81)
-#define SYM_S           (0x57)
-#define SYM_T           (0x87)
-#define SYM_U           (0xCE)
-#define SYM_V           (0x00)  /* unimplemented */
-#define SYM_W           (0x00)  /* unimplemented */
-#define SYM_X           (0x00)  /* unimplemented */
-#define SYM_Y           (0x4F)
-#define SYM_Z           (0x00)  /* unimplemented */
-
+#include "../lib/ttlcpuio.h"
 
 
 memset (char start, char c, char size) {
@@ -88,25 +35,25 @@ msg () {
 
 mem_monitor (char addr) {
     memset(0, 0, 8);
-    disp_push(SYM_0);
-    disp_push(SYM_0);
+    disp_push(SYM_LEFT_BRACKET);
     disp_push(hex2sym(addr >> 4));
     disp_push(hex2sym(addr));
+    disp_push(SYM_RIGHT_BRACKET);
     disp_push(SYM_SPACE);
     disp_push(SYM_SPACE);
     disp_push(hex2sym(*(addr) >> 4));
-    disp_push(hex2sym(*(addr)));
+    disp_push(hex2sym(*(addr)) + SYM_DP);
 }
-
 
 
 main () {
     char memaddr;
+
     memaddr = 0;
     msg();
     while (1) {
-
         char c;
+
         c = das(); /* Display And Scan */
         if (c == 0x0F) {
             memset(0, 0, 8);
