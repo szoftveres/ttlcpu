c86ecc2c993eb1fe906b342616e1d1250184071b
Szoftveres <my@email.com>
2017-04-02 11:21:34 -0700

    added lib funcs to example.c

--- example.c
+++ example.c
@@ -3,9 +3,8 @@
  * of the c-like compiler
  */
 
-
 #define NUM_TEN     0x0A
-/* You can use gnu-gcc preprocessor directives */
+/* All the #preprocessor directives are currently handled by gcc (gcc -E) */
 
 
 /* Global variables */
@@ -13,8 +12,9 @@ static var_glb;
 /* are reset to zero before main function execution begins */
 
 /*
- * There are no data types, everything is an 8-bit word
- * Every function returns a value, there's no 'void' function
+ * There are no data types, everything is an 8-bit 'word'
+ * Every function returns a value via the accumulator, there's
+ * no 'void' function
  */
 addition (b1, b2) {
     return b1 + b2;
@@ -23,16 +23,17 @@ addition (b1, b2) {
      * the function; it's used purely for program flow control.
      * The return value of a function is always the value
      * of the last evaluated expression.
-     * The return statement can be omitted in this case, the body of the function
-     * could be rewritten as a single 'b1 + b2;' statment, the result would be the same.
+     * The return statement can be omitted in this case, the body
+     * of the function could be replaced with a single 'b1 + b2;' statment,
+     * the result would be the same.
      * See the 'dec' function below, which doesn't utilize the return statement;
      */
 }
 
 /*
- * Function arguments are passed via the (emulated) stack, the
- * accumulator contains the return value (which is the result of
- * the last expression).
+ * Function arguments are passed via the (emulated) stack; the
+ * accumulator contains the return value, which is the result of
+ * the last expression.
  *
  * For stack emulation implementation details,
  * see mcc/arch/ttlcpu/header.asm
@@ -72,29 +73,32 @@ main () {
     auto i;
     static g;
 
-    /*
-     * Built-in operator precedence can be overriden
-     * with () parentheses
-     */
-    for (i = NUM_TEN; (i - 1 != 0xFF - (1 == 2)) ? 1 : 0; i += addition(1, 0)) {
-        auto i_p;
+    while (1) {
         /*
-         * there's neither 'pointer' type, nor
-         * pointer arythmetics; remember, everything is a word
+         * Built-in operator precedence can be overriden
+         * with () parentheses
          */
-        i_p = &i;
-        *i_p += 1;
-        invert_out(*i_p);
-        var_glb += 1;
+        for (i = NUM_TEN; (i - 1 != 0xFF - (1 == 2)) ? 1 : 0; i += addition(1, 0)) {
+            auto i_p;
+            /*
+             * there's neither 'pointer' type, nor
+             * pointer arythmetics; everything is a 8-bit 'word'
+             */
+            i_p = &i;  /* addressof */
+            *i_p += 1; /* dereference */
+            invert_out(*i_p);
+            var_glb += 1;
+        }
+        /*
+         * Functions don't have to (and cannot) be declared
+         * prior to call, 'out_series' function is defined below;
+         * The assembler is responsible for collecting the symbols
+         * and for figuring out which subroutine to call.
+         */
+        do {
+            out_series(add(NUM_TEN, 2 << 4));
+        } while (0);
     }
-    /*
-     * Functions don't have to (and cannot) be declared
-     * prior to call, 'out_series' function is defined below;
-     * The assembler is responsible for collecting the symbols
-     * and for figuring out which subroutine to call.
-     */
-    out_series(add(NUM_TEN, 2 << 4));
-
 }
 
 
@@ -105,5 +109,67 @@ out_series (num) {
     }
     /* Arbitrary deep recursion */
     out_series(num - 1);
+}
+
+#include "misc/routines.c"
+
+/* TTL-CPU specific library functions */
+
+implemented_library_functions () {
+    auto a;
+    auto b;
+    auto c;
+    auto d;
+
+    /* memset: a:addr, b:data, c:length */
+    memset(a,b,c);
+
+    /* memcpy: a:dst, b:src, c:lenght */
+    memcpy(a,b,c);
+
+    /*
+     * display and scan
+     * waits for a keystroke while displaying the display mem
+     * on the display. The return value is the scan code of the
+     * depressed key. Handles long key depression case.
+     */
+    d = das();
+
+    /* disp: displays the display mem on the display for 'a' cycles*/
+    disp(a);
+
+    /* disp_push: push a character from the right */
+    disp_push(0xD4);
+
+    /*
+     * add with carry: 1 byte full addition with carry
+     * a: pointer to data1
+     * b: data2
+     * c: carry in (1 or 0)
+     * result is stored at the address of 'a'
+     * return value: carry_out (1 or 0)
+     */
+    d = adc(a,b,c);
+
+    /* msb: returns (a & 0x80) */
+    b = msb(a);
+
+    /*
+     * add: full addition on arbitrary long (max 256 bit) data
+     * a: pointer to data1
+     * b: pointer to data2
+     * c: length in bytes
+     * result is stored at the address of 'a'
+     */
+    add(a,b,c);
+
+    /*
+     * sub: subtraction on arbitrary long (max 256 bit) data
+     * a: pointer to data1
+     * b: pointer to data2
+     * c: length in bytes
+     * result is stored at the address of 'a'
+     */
+    sub(a,b,c);
 }
 
